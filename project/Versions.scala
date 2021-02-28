8fadd377159f10dffdeb09dede2051cbaddb966a
loustelr <dev.loustler@gmail.com>
2021-02-28 15:59:35 +0900

    wip
    wip

---
+++ project/Versions.scala
@@ -0,0 +1,11 @@
+object Versions {
+  val Spark: String = "3.0.2"
+
+  val ScalaTest: String = "3.2.3"
+
+  val ScalaCheck: String = "1.15.2"
+
+  val PicoCli: String = "4.6.1"
+
+  val Log4j: String = "2.14.0"
+}
