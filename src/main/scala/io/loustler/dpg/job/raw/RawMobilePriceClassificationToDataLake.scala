e316450d8bdeae8b1890e3437fab238ba83b011e
loustelr <dev.loustler@gmail.com>
2021-03-01 16:23:26 +0900

    wip
    wip

--- src/main/scala/io/loustler/dpg/job/raw/RawMobilePriceClassificationToDataLake.scala
+++ src/main/scala/io/loustler/dpg/job/raw/RawMobilePriceClassificationToDataLake.scala
@@ -4,6 +4,10 @@ import io.loustler.dpg.job.SparkJob
 import io.loustler.dpg.model.DataFormat
 import org.apache.spark.sql.SparkSession
 
+/** Store raw data of mobile price classification into Data Lake
+  *
+  * @see https://www.kaggle.com/radmirzosimov/telecom-users-dataset
+  */
 object RawMobilePriceClassificationToDataLake extends SparkJob {
 
   def main(args: Array[String]): Unit = {
