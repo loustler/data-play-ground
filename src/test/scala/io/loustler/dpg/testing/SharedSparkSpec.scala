022af5b47ce9acde5658af14718723d0623e8dc7
loustelr <dev.loustler@gmail.com>
2021-03-08 22:26:18 +0900

    wip
    wip

--- src/test/scala/io/loustler/dpg/testing/SharedSparkSpec.scala
+++ src/test/scala/io/loustler/dpg/testing/SharedSparkSpec.scala
@@ -2,18 +2,29 @@ package io.loustler.dpg.testing
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{ DataFrame, SparkSession }
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{ BeforeAndAfterAll, BeforeAndAfterEach }
 import org.scalatest.enablers.Emptiness
 import org.scalatest.funspec.AnyFunSpecLike
 
-trait SharedSparkSpec extends AnyFunSpecLike with TestingSpec with BeforeAndAfterAll {
+trait SharedSparkSpec extends AnyFunSpecLike with TestingSpec with BeforeAndAfterAll with BeforeAndAfterEach {
   private var _spark: SparkSession = _
+
+  protected implicit def sparkContext: SparkSession = _spark
 
   protected def sparkConf: SparkConf =
     new SparkConf()
       .set("spark.unsafe.exceptionOnMemoryLeak", "true")
       .set("spark.ui.enabled", "false")
       .set("hive.stats.jdbc.timeout", "80")
+      .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+      .set("spark.hadoop.fs.s3a.access.key", "TESTKEY")
+      .set("spark.hadoop.fs.s3a.secret.key", "TESTSECRET")
+      .set("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000")
+      .set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+      .set("spark.hadoop.fs.s3a.path.style.access", "true")
+      .set("spark.hadoop.fs.s3a.committer.name", "directory")
+      .set("spark.hadoop.fs.s3a.committer.staging.conflict-mode", "replace")
+      .set("spark.hadoop.fs.s3a.committer.staging.tmp.path", "/tmp/staging")
 
   def spark: SparkSession = _spark
 
@@ -24,10 +35,10 @@ trait SharedSparkSpec extends AnyFunSpecLike with TestingSpec with BeforeAndAfte
       .appName("testing")
       .getOrCreate()
 
-  override protected def afterAll(): Unit = {
-    _spark.close()
-    SparkSession.clearActiveSession()
-    SparkSession.clearDefaultSession()
+  override protected def afterEach(): Unit = {
+//    _spark.stop()
+    sparkContext.sharedState.cacheManager.clearCache()
+    sparkContext.sessionState.catalog.reset()
   }
 
   implicit val EmptinessForDataFrame: Emptiness[DataFrame] = new Emptiness[DataFrame] {
